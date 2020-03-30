94fd4946aa8c4886cd493754309c42e70eaf8336
Dreggh <62384588+Dreggh@users.noreply.github.com>
2020-03-31 00:24:52 +0200

    Add files via upload

---
+++ Windows based/Chkdsk_On_Every_Disk.ps1
@@ -0,0 +1,11 @@
+﻿$Drives = Get-Partition | Select DriveLetter,Type
+
+foreach ($Drive in $Drives)
+    { 
+        if ($Drive.Type -eq 'Basic' -Or $Drive.Type -eq 'IFS')
+            {
+            ## Write-Host $Drive.DriveLetter $Drive.Type
+               $Swap = $Drive.DriveLetter+':'
+               chkdsk $Swap
+            }
+    }
